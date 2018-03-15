f81374082172798f53bfb9e92483dd3a0f9e5138
invasorMou <shopir9@hotmail.com>
2018-03-15 10:59:11 -0500

    added more functionalities to the calculator class, substraction, multiplycation, division, power and roots

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,19 @@ class Calculator
 	def self.add(arg1,arg2)
 		arg1+arg2		
 	end
+	def self.sub(arg1,arg2)
+		arg1-arg2		
+	end
+	def self.mul(arg1,arg2)
+		arg1*arg2		
+	end
+	def self.div(arg1,arg2)
+		arg1.to_f/arg2		
+	end
+	def self.pow(arg1,arg2)
+		arg1**arg2		
+	end
+	def self.root(arg1,arg2=2)
+		arg1**(1/arg2.to_f)		
+	end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -5,5 +5,48 @@ describe Calculator  do
 		it 'should return sum of two integers' do 
 			expect(Calculator.add(7,7)).to eq 14
 		end
+		it 'should return an integer or a float' do
+			expect(Calculator.add(10,7)).to be_a Integer
+		end
+	end
+	describe '::sub' do
+		it 'should return substraction of two integers' do 
+			expect(Calculator.sub(10,7)).to eq 3
+		end
+		it 'should return an integer or a float' do
+			expect(Calculator.sub(10,7)).to be_a Integer
+		end
+	end
+	describe '::mul' do
+		it 'should return multiplication of two integers' do 
+			expect(Calculator.mul(4,2)).to eq 8
+		end
+		it 'should return an integer or a float' do
+			expect(Calculator.mul(10,7)).to be_a Integer
+		end
+	end
+	describe '::div' do
+		it 'should return division of two integers' do 
+			expect(Calculator.div(4,2)).to eq 2
+		end
+		it 'should return an integer or a float' do
+			expect(Calculator.div(10,7)).to be_a Float
+		end
+	end
+	describe '::pow' do
+		it 'should return a integer power by other integer ' do 
+			expect(Calculator.pow(4,2)).to eq 16
+		end
+		it 'should return an integer or a float' do
+			expect(Calculator.pow(10,7)).to be_a Integer
+		end
+	end
+	describe '::root' do
+		it 'should return the nth root of an integer' do 
+			expect(Calculator.root(4,2)).to eq 2
+		end
+		it 'should return an integer or a float' do
+			expect(Calculator.root(4,2)).to be_a Float
+		end
 	end
 end
